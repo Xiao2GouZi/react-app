fdb1c97c91dfe51c663e3d7c2b65a4a794f9f316
齐云猛(18132240) <18132240@suning.com>
2021-05-24 22:46:44 +0800

    测试

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import {createBrowserHistory} from "history";
-import { Route, Switch ,} from "react-router";
+import { Route, Switch} from "react-router";
 import {ConnectedRouter} from 'react-router-redux';
 
 const history = createBrowserHistory();
@@ -12,6 +12,10 @@ import '&/styles/index.less'
 
 import Home from './app/home'
 import Login from './app/login'
+
+let _store = Store(history)
+
+console.log(' ------ > ', _store)
 
 ReactDOM.render(
     <Provider store={Store(history)}>
